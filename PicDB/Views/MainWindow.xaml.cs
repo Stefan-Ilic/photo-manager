49b9744c6da918963457b4d70663598989a03cee
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-02-04 10:09:08 +0100

    Use Prism's ViewModelLocator

---
+++ PicDB/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace PicDB.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Default constructor for the main window codebehind
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
