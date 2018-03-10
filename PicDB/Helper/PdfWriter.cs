85e69865416d9a40ff609b560d00eb8149bb7ff8
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-03-10 18:12:33 +0100

    Create PdfWriter Helper class

---
+++ PicDB/Helper/PdfWriter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace PicDB.Helper
+{
+    /// <summary>
+    /// Helper class for writing PDFs using PdfSharp
+    /// </summary>
+    public class PdfWriter
+    {
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        public PdfWriter(string title)
+        {
+            Pdf.Info.Title = title;
+            Page = Pdf.AddPage();
+            Gfx = XGraphics.FromPdfPage(Page);
+        }
+
+        /// <summary>
+        /// Tracks the line height
+        /// </summary>
+        private int _tracker = 20;
+
+        /// <summary>
+        /// The pdf document object
+        /// </summary>
+        public PdfDocument Pdf { get; set; } = new PdfDocument();
+
+        /// <summary>
+        /// Used for drawing on pdf
+        /// </summary>
+        public XGraphics Gfx { get; set; }
+
+        /// <summary>
+        /// The curret page in the pdf
+        /// </summary>
+        public PdfPage Page { get; set; }
+
+        /// <summary>
+        /// Title Font
+        /// </summary>
+        public XFont TitleFont { get; set; } = new XFont("Arial", 20, XFontStyle.Bold);
+
+        /// <summary>
+        /// Heading1 Font
+        /// </summary>
+        public XFont Heading1Font { get; set; } = new XFont("Arial", 15, XFontStyle.Bold);
+
+        /// <summary>
+        /// Standard Font
+        /// </summary>
+        public XFont StandardFont { get; set; } = new XFont("Arial", 12);
+
+        /// <summary>
+        /// Writes title
+        /// </summary>
+        /// <param name="title"></param>
+        public void Title(string title)
+        {
+            Gfx.DrawString(title,
+                TitleFont, XBrushes.Black, new XRect(0, _tracker, Page.Width, Page.Height),
+                XStringFormats.TopCenter);
+            _tracker += 50;
+        }
+
+        /// <summary>
+        /// saves the pdf (you need to add pdf extension yourself)
+        /// </summary>
+        /// <param name="file"></param>
+        public void Save(string file)
+        {
+            Pdf.Save(file);
+        }
+
+        /// <summary>
+        /// Writes an Image to PDF
+        /// </summary>
+        /// <param name="path"></param>
+        public void Image(string path)
+        {
+            //Image
+            var image = XImage.FromFile(path);
+            var scalingFactor = (decimal)image.PixelHeight / 800;
+            var newWidth = Convert.ToInt32(image.PixelWidth * scalingFactor);
+            var newHeight = Convert.ToInt32(image.PixelHeight * scalingFactor);
+            var startingPoint = (595 / 2) - (newWidth / 2); // 595 is page width in pixels
+            Gfx.DrawImage(image, startingPoint, _tracker, newWidth, newHeight);
+            _tracker += newHeight + 10;
+        }
+
+        /// <summary>
+        /// Writes Heading 1 to pdf
+        /// </summary>
+        /// <param name="heading"></param>
+        public void Heading1(string heading)
+        {
+            _tracker += 10;
+            Gfx.DrawString(heading,
+                Heading1Font, XBrushes.Black, new XRect(50, _tracker, Page.Width, Page.Height),
+                XStringFormats.TopLeft);
+            _tracker += 20;
+        }
+
+        /// <summary>
+        /// Writes a paragraph to pdf
+        /// </summary>
+        /// <param name="text"></param>
+        public void Line(string text)
+        {
+            Gfx.DrawString(text,
+                StandardFont, XBrushes.Black, new XRect(50, _tracker, Page.Width, Page.Height),
+                XStringFormats.TopLeft);
+            _tracker += 20;
+        }
+    }
+}
--- PicDB/Reporting/Report.cs
+++ PicDB/Reporting/Report.cs
@@ -9,6 +9,7 @@ using System.Windows.Documents;
 using BIF.SWE2.Interfaces.ViewModels;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
+using PicDB.Helper;
 
 namespace PicDB.Reporting
 {
@@ -23,77 +24,25 @@ namespace PicDB.Reporting
         /// <param name="pic"></param>
         public static void Create(IPictureViewModel pic)
         {
-            // Create a new PDF document
-            var pdf = new PdfDocument();
-            pdf.Info.Title = $"Report of {pic.FileName}";
+            var pdf = new PdfWriter("Report");
+            pdf.Title($"Report of {pic.FileName}");
 
-            // Create a new PDF document
-            var page = pdf.AddPage();
+            pdf.Image(pic.FilePath);
 
-            // Get an XGraphics object for drawing
-            var gfx = XGraphics.FromPdfPage(page);
+            pdf.Heading1("EXIF Information");
+            pdf.Line($"Make: {pic.EXIF.Make}");
+            pdf.Line($"f-Number: {pic.EXIF.FNumber}");
+            pdf.Line($"Exposure Time: {pic.EXIF.ExposureTime}");
+            pdf.Line($"Flash: {pic.EXIF.Flash}");
+            pdf.Line($"ISO Value: {pic.EXIF.ISOValue}");
+            pdf.Line($"Exposure Program: {pic.EXIF.ExposureProgram}");
 
-            // Create fonts
-            var titleFont = new XFont("Arial", 20, XFontStyle.Bold);
-            var h2Font = new XFont("Arial", 15, XFontStyle.Bold);
-            var standardFont = new XFont("Arial", 12);
-            // Title
-            gfx.DrawString($"Report of {pic.FileName}",
-                titleFont, XBrushes.Black, new XRect(0, 10, page.Width, page.Height),
-                XStringFormats.TopCenter);
-
-            //Image
-            var image = XImage.FromFile(pic.FilePath);
-            var scalingFactor = (decimal)image.PixelHeight / 800;
-            var newWidth = Convert.ToInt32(image.PixelWidth * scalingFactor);
-            var newHeight = Convert.ToInt32(image.PixelHeight * scalingFactor);
-            var startingPoint = (595 / 2) - (newWidth / 2); // 595 is page width in pixels
-            gfx.DrawImage(image, startingPoint, 80, newWidth, newHeight);
-
-            //EXIF
-            gfx.DrawString("EXIF Information",
-                h2Font, XBrushes.Black, new XRect(50, 350, page.Width, page.Height),
-                XStringFormats.TopLeft);
-
-            gfx.DrawString($"Make: {pic.EXIF.Make}",
-                standardFont, XBrushes.Black, new XRect(50, 370, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"F-Number: {pic.EXIF.FNumber}",
-                standardFont, XBrushes.Black, new XRect(50, 390, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Exposure Time: {pic.EXIF.ExposureTime}",
-                standardFont, XBrushes.Black, new XRect(50, 410, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"ISO Value: {pic.EXIF.ISOValue}",
-                standardFont, XBrushes.Black, new XRect(50, 430, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Flash: {pic.EXIF.Flash}",
-                standardFont, XBrushes.Black, new XRect(50, 450, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Exposure Program: {pic.EXIF.ExposureProgram}",
-                standardFont, XBrushes.Black, new XRect(50, 470, page.Width, page.Height),
-                XStringFormats.TopLeft);
-
-            //IPTC
-            gfx.DrawString("IPTC Information",
-                h2Font, XBrushes.Black, new XRect(50, 500, page.Width, page.Height),
-                XStringFormats.TopLeft);
-
-            gfx.DrawString($"Keywords: {pic.IPTC.Keywords}",
-                standardFont, XBrushes.Black, new XRect(50, 520, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"By-Line: {pic.IPTC.ByLine}",
-                standardFont, XBrushes.Black, new XRect(50, 540, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Copyright Notice: {pic.IPTC.CopyrightNotice}",
-                standardFont, XBrushes.Black, new XRect(50, 560, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Headline: {pic.IPTC.Headline}",
-                standardFont, XBrushes.Black, new XRect(50, 580, page.Width, page.Height),
-                XStringFormats.TopLeft);
-            gfx.DrawString($"Caption: {pic.IPTC.Caption}",
-                standardFont, XBrushes.Black, new XRect(50, 600, page.Width, page.Height),
-                XStringFormats.TopLeft);
+            pdf.Heading1("IPTC Information");
+            pdf.Line($"Keywords: {pic.IPTC.Keywords}");
+            pdf.Line($"Caption: {pic.IPTC.Caption}");
+            pdf.Line($"By-Line: {pic.IPTC.ByLine}");
+            pdf.Line($"Copyright Notice: {pic.IPTC.CopyrightNotice}");
+            pdf.Line($"Headline: {pic.IPTC.Headline}");
 
             // Save the document...
             var pdfName = $"{pic.FileName.TrimEnd(".jpg".ToCharArray())}.pdf";
